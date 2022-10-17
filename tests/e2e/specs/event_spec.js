48d22adbd9e268b68fb67de7dc34a3e95cf5c9f3
Juraci <juraci.vieira@gmail.com>
2022-10-17 16:54:33 -0300

    TDD API

--- tests/e2e/specs/event_spec.js
+++ tests/e2e/specs/event_spec.js
@@ -1,14 +1,52 @@
 // https://docs.cypress.io/api/table-of-contents
 
 describe('Event cards', () => {
-  it('displays the event details', () => {
-    cy.visit('/');
+  context('when the API response is successfull', () => {
+    it('displays the event details', () => {
+      const events = [
+        {
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event',
+          location: 'Meow town',
+          date: 'January 28, 2023',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee',
+        },
+      ];
 
-    cy.get('.event-card').eq(0).as('firstCard');
+      cy.intercept(
+        {
+          method: 'GET',
+          url: '**/events',
+        },
+        events
+      ).as('getEvents');
 
-    cy.get('@firstCard').within(() => {
-      cy.get('span').should('have.text', '@ 12:00 on January 28, 2023');
-      cy.get('h4').should('have.text', 'Cat Adoption Day');
+      cy.visit('/');
+
+      cy.wait('@getEvents');
+
+      cy.get('.event-card').should('have.length', 1);
+      cy.get('.event-card').eq(0).as('firstCard');
+
+      cy.get('@firstCard').within(() => {
+        cy.get('span').should('have.text', '@ 12:00 on January 28, 2023');
+        cy.get('h4').should('have.text', 'Cat Adoption Day');
+      });
+    });
+  });
+
+  context('when the API response errors out', () => {
+    it('displayes nothing', () => {
+      cy.intercept('GET', '**/events', { forceNetworkError: true }).as('getEvents');
+      cy.visit('/');
+
+      cy.wait('@getEvents');
+
+      cy.get('.event-card').should('have.length', 0);
     });
   });
 });
